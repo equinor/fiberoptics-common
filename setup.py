03ac645b5cf86d2f6b4a6e6227afe8726bd6b580
Helge Hatteland <hhat@equinor.com>
2022-11-01 10:22:55 +0000

    Added test coverage check to CI/CD workflow

--- setup.py
+++ setup.py
@@ -11,18 +11,19 @@ extras_require = {
 extras_require["all"] = [pkg for pkgs in extras_require.values() for pkg in pkgs]
 
 extras_require["dev"] = [
-    *extras_require["all"],
     "black",
     "bumpversion",
     "flake8",
+    "isort",
+    "pytest-cov",
+    "pytest-mock",
     "pytest",
-    "pytest_mock",
 ]
 
 setup(
     name="fiberoptics-common",
     version="1.6.3",
-    packages=PEP420PackageFinder.find(include=["fiberoptics.*"]),
+    packages=PEP420PackageFinder.find(include=["fiberoptics.common*"]),
     install_requires=["pandas"],
     extras_require=extras_require,
 )
