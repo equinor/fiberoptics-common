c6eda5cd5f88538c9e7af5438e3c116d1b4fb0e2
Helge Hatteland <hhat@equinor.com>
2022-11-03 08:33:14 +0000

    Added custom css to fix table overflow

--- docs/conf.py
+++ docs/conf.py
@@ -55,5 +55,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
