5450d85cc9adc14606d13d5e657f19a1b1abf9f7
Valerii Martell <vamar@equinor.com>
2025-11-18 16:19:15 +0100

    Remove mutable collection as a default value of an argument

--- fiberoptics/common/auth/_deprecated.py
+++ fiberoptics/common/auth/_deprecated.py
@@ -6,7 +6,7 @@ from fiberoptics.common.auth._sync import Credential
 logger = logging.getLogger("fiberoptics.common")
 
 
-def get_default_credential(name: str = None, scopes: list[str] = [], **kwargs):
+def get_default_credential(name: str = None, scopes: list[str] | None = None, **kwargs):
     """Retrieves default credential.
     Deprecated: get_default_credential is deprecated and will be removed in future versions. Use AsyncCredential or Credential classes directly.
     """
@@ -14,6 +14,9 @@ def get_default_credential(name: str = None, scopes: list[str] = [], **kwargs):
         "Deprecated: get_default_credential is deprecated and will be removed in future versions. \
                    Use AsyncCredential or Credential classes directly."
     )
+
+    if scopes is None:
+        scopes = []
 
     if "tenant_id" not in kwargs:
         # This is not a secret and can be found on https://www.whatismytenantid.com/
